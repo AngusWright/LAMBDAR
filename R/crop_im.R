9e5890089b74be0824b30a3d49e30cd2f8ce4961
Angus H Wright <angus.wright@icrar.org>
2014-02-07 14:40:33 +0800

    Fixed bug where cutouts with a large radius can have astrometric misalignment towards the image perimeter.

--- R/crop_im.R
+++ R/crop_im.R
@@ -147,10 +147,8 @@ function(ra0=-999,dec0=-999,pathroot="",inpim=NA,cutrad=1,fitsoutname=NA){
       #Update header values for new image {{{
       header[which(header[,'key']=='NAXIS1'),'value']<-paste(ncol[i])
       header[which(header[,'key']=='NAXIS2'),'value']<-paste(nrow[i])
-      header[which(header[,'key']=='CRVAL1'),'value']<-paste(ra0[i])
-      header[which(header[,'key']=='CRVAL2'),'value']<-paste(dec0[i])
-      header[which(header[,'key']=='CRPIX1'),'value']<-paste(cutxcen[i])
-      header[which(header[,'key']=='CRPIX2'),'value']<-paste(cutycen[i])
+      header[which(header[,'key']=='CRPIX1'),'value']<-header[which(header[,'key']=='CRPIX1'),'value']-lowerx
+      header[which(header[,'key']=='CRPIX2'),'value']<-header[which(header[,'key']=='CRPIX2'),'value']-lowery
       #}}}
       #Write the new header to file {{{
       pipe<-file(inpim[i], 'rb')
