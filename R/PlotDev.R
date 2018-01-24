6940df7b58b9e1be2f0c3d8a46309ae407f30b01
Angus H Wright <awright@uni-bonn.de>
2018-01-24 16:22:09 +0100

    Remove Cairo support; no longer needed

--- R/PlotDev.R
+++ R/PlotDev.R
@@ -1,34 +1,30 @@
 #
-# Simple wrapper to CairoPNG and png that 
-# determines which to use:
-#   Preferentially CairoPNG if available.
+# Simple wrapper to various plot devices  
 #
 
 PlotDev<-function(file='default.X11',height=6,width=6,units='in',res=120,...) {
    
-  #If Cairo is available
-  if (requireNamespace('Cairo',quietly=TRUE)) {
-    if (grepl('X11',file,fixed=TRUE,ignore.case=TRUE)) { 
-      #Open CairoX11
-      opened=Cairo::Cairo(type='X11',height=height,width=width,units=units,...)
-    } else if (grepl('.pdf',file,fixed=TRUE,ignore.case=TRUE)) { 
-      #Open CairoPDF
-      opened=Cairo::Cairo(type='PDF',file=file,height=height,width=width,units=units,...)
-    } else if (grepl('.png',file,fixed=TRUE,ignore.case=TRUE)) { 
-      #Open CairoPNG
-      opened=Cairo::Cairo(type='PNG',filename=file,height=height,width=width,units=units,res=res,...)
+  ulist<-c('in','px','cm','mm')
+  if (!units%in%ulist) { stop(paste0("Unit '",units,"' not recognised (must be one of in/px/cm/mm)")) } 
+  conv<-c(1,1/res,1/2.54,1/25.4)
+  if (grepl('X11',file,fixed=TRUE,ignore.case=TRUE)) { 
+    #Open X11
+    if (units!='in') {
+      height=conv[which(ulist==units)]*height
+      width=conv[which(ulist==units)]*width
     }
-  } else {
-    if (grepl('X11',file,fixed=TRUE,ignore.case=TRUE)) { 
-      #Open CairoX11
-      opened=X11(height=height,width=width,units=units,...)
-    } else if (grepl('.pdf',file,fixed=TRUE,ignore.case=TRUE)) { 
-      #Open CairoPDF
-      opened=pdf(file=file,height=height,width=width,...)
-    } else if (grepl('.png',file,fixed=TRUE,ignore.case=TRUE)) { 
-      #Open CairoPNG
-      opened=png(filename=file,height=height,width=width,units=units,res=res,...)
+    opened=X11(height=height,width=width,...)
+  } else if (grepl('.pdf',file,fixed=TRUE,ignore.case=TRUE)) { 
+    #Open PDF
+    if (units!='in') {
+      height=conv[which(ulist==units)]*height
+      width=conv[which(ulist==units)]*width
     }
+    opened=pdf(file=file,height=height,width=width,...)
+  } else if (grepl('.png',file,fixed=TRUE,ignore.case=TRUE)) { 
+    #Open PNG
+    opened=png(filename=file,height=height,width=width,units=units,res=res,...)
   }
+  
   return=opened
 }
